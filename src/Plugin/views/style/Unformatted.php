ac47b7b1d0891c9032e599a8dd7cb9192d558cc6
John Ross <johnross@prometsource.com>
2016-02-05 15:39:15 +0800

    Adding options to the format and "use".

--- src/Plugin/views/style/Unformatted.php
+++ src/Plugin/views/style/Unformatted.php
@@ -7,6 +7,8 @@
 
 namespace Drupal\views_pdf\Plugin\views\style;
 
+use Drupal\views\Plugin\views\style\StylePluginBase;
+
 /**
  * Unformatted style plugin.
  *
@@ -18,10 +20,24 @@ namespace Drupal\views_pdf\Plugin\views\style;
  *   id = "views_pdf_unformatted",
  *   title = @Translation("Unformatted PDF"),
  *   help = @Translation("Displays rows one after another."),
- *   theme = "views_view_unformatted",
+ *   theme = "views_pdf_unformatted",
  *   display_types = {"normal"}
  * )
  */
 class Unformatted extends StylePluginBase {
+
+  /**
+   * Does the style plugin allows to use style plugins.
+   *
+   * @var bool
+   */
+  protected $usesRowPlugin = TRUE;
+
+  /**
+   * Does the style plugin support custom css class for the rows.
+   *
+   * @var bool
+   */
+  protected $usesRowClass = TRUE;
 
 }
